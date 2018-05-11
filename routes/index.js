63e245c1b07c2fe5155785682a163d8f042a06ff
Daniel <daniel@Daniels-MacBook-Pro.local>
2018-05-11 09:26:36 +0300

    Replace sendmail with nodemailer

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,43 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const sendmail = require('sendmail')();
+var nodemailer = require('nodemailer');
+var secrets = require('../secrets/secrets.js');
 
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: secrets.gmailAddress,
+        pass: secrets.gmailPass
+    }
+});
 
 router.get('/', (req, res) => {
     res.status(202).sendFile(path.resolve("index.html"));
 })
 
-router.get('/resume', (req, res) => {
-    res.send('/public/DanielDeLaRosaResume2018.pdf');
-})
-
 router.post('/email', (req, res) => {
-    sendmail({
-        from: 'hello@dela1000.com',
-        to: 'dela1000@gmail.com',
-        subject: 'dela1000 website inquiry',
-        html: 'Name: ' + req.body.name + ', ' + 'Email: ' + req.body.email + ', ' + 'Message: ' + req.body.message,
-    }, function(err, reply) {
+    var mailOptions = {
+        from: req.body.email,
+        to: secrets.gmailAddress,
+        subject: 'Nessage from ' + req.body.name + ' at ' + req.body.email,
+        text: 'Name: ' + req.body.name + ', ' + 'Email: ' + req.body.email + ', ' + 'Message: ' + req.body.message
+    }
+
+    transporter.sendMail(mailOptions, function(err, response) {
         if (err) {
+            console.log("err");
+            console.log(err);
             console.log(err && err.stack);
             res.status(401).json({
-                message: 'email not sent'
+                message: 'email not sent',
+                err: err
             })
-        }
-        if (reply) {
+        } else {
+            console.log("Message sent");
             res.status(200).json({
-                message: 'Email sent'
+                message: 'Email sent',
+                response: response
             })
         }
     });
