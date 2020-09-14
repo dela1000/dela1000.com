5dd34ff1bc8b2d929dbf23b44b8f19f2959e5b2e
daniel <dela1000@gmail.com>
2020-09-14 16:14:04 -0700

    add ipstack

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const path = require('path');
 const nodemailer = require('nodemailer');
 const secrets = require('../secrets/secrets.js');
+const https = require('https');
 
 const transporter = nodemailer.createTransport({
     service: 'Gmail',
@@ -19,33 +20,60 @@ router.get('/', (req, res) => {
 
 router.post('/email', (req, res) => {
     const clientIp = req.clientIp;
-    console.log("+++ 24 index.js clientIp: ", clientIp)
+    console.log("+++ 24 index.js clientIp: ", clientIp);
 
-    const mailOptions = {
-        from: req.body.email,
-        to: secrets.gmailAddress,
-        subject: 'Message from ' + req.body.name + ' at ' + req.body.email  + ' from ' + req.body.source,
-        text: ' Name: ' + req.body.name + '\n Email: ' + req.body.email  + '\n IP Address: ' + clientIp + '\n\n Message: ' + req.body.message
+    if (clientIp.substr(0, 7) == "::ffff:") {
+      clientIp = clientIp.substr(7)
     }
-    console.log("+++ 30 index.js mailOptions: ", mailOptions)
-    transporter.sendMail(mailOptions, function(err, response) {
-        console.log("+++ 28 index.js response: ", response)
-        console.log("+++ 29 index.js err: ", err)
-        if (err) {
-            console.log("err");
-            console.log(err);
-            console.log(err && err.stack);
-            res.status(401).json({
-                message: 'email not sent',
-                err: err
-            })
-        } else {
-            console.log("Message sent");
-            res.status(200).json({
-                message: 'Thank you for the message. I will get back to you very soon.',
-                response: response
-            })
+
+    var ipstackLink = 'https://api.ipstack.com/' + clientIp + '?access_key=secrets.ipstack';
+
+    https.get(ipstackLink, (resp) => {
+      let data = '';
+
+      // A chunk of data has been recieved.
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      // The whole response has been received. Print out the result.
+      resp.on('end', () => {
+        var ipLocationData = JSON.parse(data).explanation;
+
+        var textData = ' Name: ' + req.body.name + '\n Email: ' + req.body.email  + '\n IP Address: ' + clientIp + '\n\n Message: ' + req.body.message;
+
+        if(ipLocationData){
+            textData = text + '\n\n' + ipLocationData;
         }
+
+        const mailOptions = {
+            from: req.body.email,
+            to: secrets.gmailAddress,
+            subject: 'Message from ' + req.body.name + ' at ' + req.body.email  + ' from ' + req.body.source,
+            text: textData
+        }
+        console.log("+++ 30 index.js mailOptions: ", mailOptions)
+        transporter.sendMail(mailOptions, function(err, response) {
+            if (err) {
+                console.log("err");
+                console.log(err);
+                console.log(err && err.stack);
+                res.status(401).json({
+                    message: 'email not sent',
+                    err: err
+                })
+            } else {
+                console.log("Message sent");
+                res.status(200).json({
+                    message: 'Thank you for the message. I will get back to you very soon.',
+                    response: response
+                })
+            }
+        });
+      });
+
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
     });
 });
 
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const requestIp = require('request-ip');
 
+// Get user's IP address
 app.use(requestIp.mw())
 
 // Routes file
