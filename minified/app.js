63d114d0eefe98d041b845d66bf21fd6df6a86b5
daniel <dela1000@gmail.com>
2020-09-14 16:15:09 -0700

    update app.js

--- minified/app.js
+++ minified/app.js
@@ -103,6 +103,7 @@ App.config(setupConfig = ($locationProvider, $httpProvider) => {
     $httpProvider.interceptors.push(interceptHttp);
 })
 let contact = angular.module('contact', []);
+let resume = angular.module('resume', []);
 App.config(function ($stateProvider, $urlRouterProvider) {
     
     $urlRouterProvider.when('', '/')
@@ -136,7 +137,6 @@ App.config(function ($stateProvider, $urlRouterProvider) {
         })
 
 })
-let resume = angular.module('resume', []);
 var App = angular.module('App');
 
 App.factory('alert', () => {
