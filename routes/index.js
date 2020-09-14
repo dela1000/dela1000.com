165bfe7916ae1d33e41ba6d67b332aabb7842eaf
daniel <dela1000@gmail.com>
2020-09-14 16:51:52 -0700

    clean up

--- routes/index.js
+++ routes/index.js
@@ -40,7 +40,7 @@ router.post('/email', (req, res) => {
       // The whole response has been received. Print out the result.
       resp.on('end', () => {
 
-        var textData = ' Name: ' + req.body.name + '\n Email: ' + req.body.email  + '\n\n Message: ' + req.body.message  + '\n\n IP Address: ' + clientIp;
+        var textData = ' Name: ' + req.body.name + '\n Email: ' + req.body.email  + '\n\n Message: ' + req.body.message  + '\n\n\n IP Address: ' + clientIp;
 
         if(data){
             data = JSON.parse(data);
